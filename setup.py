d599f68edbe85c214854362f75ca2736a8ce329d
Srinath Murali <142419722+sensyble@users.noreply.github.com>
2024-08-17 23:51:43 +0530

    fixed setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'playlist-scraper=scraper.cli:main',
+            'playlist-scraper=scrapper.cli:main',
         ],
     },
 )
