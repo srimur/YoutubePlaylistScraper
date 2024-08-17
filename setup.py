f9210470246b2086f8ac19eff4fd9e91bf20a38a
Srinath Murali <142419722+sensyble@users.noreply.github.com>
2024-08-18 01:47:35 +0530

    fix setup

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setup(
     name="playlist-scraper",
     version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'rich',
+        'pytube',
+    ],
     entry_points={
         'console_scripts': [
             'playlist-scraper=scraper.cli:main',
